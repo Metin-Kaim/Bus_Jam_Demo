b8a9f92f47c8cc6500a4bcfa171e60c751cce354
MetinKaim <metinkaim0@gmail.com>
2024-06-14 17:23:47 +0300

    Added stock area

---
+++ Assets/Scripts/RunTime/Handlers/StockHandler.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace RunTime.Handlers
+{
+    public class StockHandler : MonoBehaviour
+    {
+        private bool _isFull;
+        private bool _isOpen;
+
+        public bool IsFull { get => _isFull; set => _isFull = value; }
+        public bool IsOpen { get => _isOpen; set => _isOpen = value; }
+    }
+}
